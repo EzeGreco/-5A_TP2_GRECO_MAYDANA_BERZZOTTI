a0d7304753f982637bc692f07cef3b422dbad4df
EzeGreco <egreco@pioix.edu.ar>
2024-06-02 22:30:45 -0300

    Gran avance
    FALTA HACER
    17 y 29
    Transferencia
    22 y 23
    Listados mayor /
    menor
    25, 26, 27 y 28
    Listados con tablas

--- classes/client.js
+++ classes/client.js
@@ -6,10 +6,9 @@ class Client{
      * @param {*} password PASS
      * @param {*} name NAME
      * @param {*} surname SURNAME
-     * @param {*} descubierto DESCUBIERTO
-     * @param {*} tieneCajaDolares TIENECAJADOLARES
+     * @param {*} ingresosAnuales INGRESOS AL AÑO
      */
-    constructor(dni, password, name, surname, descubierto,tieneCajaDolares){
+    constructor(dni, password, name, surname, ingresosAnuales){
         this.id=idClient;
         idClient++;
         this.dni=dni;
@@ -17,27 +16,27 @@ class Client{
         this.name=name;
         this.surname=surname;
         this.cajaAhorroPesos=0;
-        this.descubierto=descubierto;
-        this.descubiertoUsado=0
+        this.descubierto=ingresosAnuales/120;//política bancaria-> Descubierto = 10% de ganancias mensuales
         this.tarjetaAsociada= 2132432654544567+this.id;
-        if (tieneCajaDolares==true){;
+        if (ingresosAnuales>=30000000){; //política bancaria-> 30 palos o más al año = caja de ahorro en dolares
             this.cajaAhorroDolares=0;
         } else{;
             this.cajaAhorroDolares=-1;
         };
         this.fecha=new Date();
-        this.fechaVencimientoTargeta= new Date(this.fecha.getFullYear()+3,this.fecha.getMonth(),this.fecha.getDate());
+        this.fechaVencimientoTarjeta= new Date(this.fecha.getFullYear()+3,this.fecha.getMonth(),this.fecha.getDate());
+        this.ingresosAnuales=ingresosAnuales
     }
-    //ejercicio12
+    //ejercicio12 FUNCIONA
     extraerDinero(monto,caja){
-        if (caja==="pesos"){
-            if (this.cajaAhorroPesos+(this.descubierto-this.descubiertoUsado)>=monto){
-                this.cajaAhorroDolares-=monto
+        if (caja==="pesos" && monto>0){
+            if (this.cajaAhorroPesos+this.descubierto>=monto){
+                this.cajaAhorroPesos-=monto
                 return true
             }else{
                 return false
                 }
-        }else if (caja==="dolares"){
+        }else if (caja==="dolares" && monto>0){
             if (this.cajaAhorroDolares>=0 && this.cajaAhorroDolares>=monto){
                 this.cajaAhorroDolares-=monto
                 return true
@@ -51,18 +50,20 @@ class Client{
 
     //ejercicio13 
     ingresarDinero(monto, caja) {
-            if (caja === "pesos") {
-                this.cajaAhorroPesos += monto;
-                return this.cajaAhorroPesos;
-            } else if (caja === "dolares") {
-                if (this.cajaAhorroDolares >= 0) {
-                    this.cajaAhorroDolares += monto;
-                    return this.cajaAhorroDolares;
-                } else {
-                    return -1; // No tiene caja de ahorro en dólares
-                }
+        if (caja === "pesos" && monto>0) {
+            console.log(monto)
+            this.cajaAhorroPesos += monto;
+            console.log(this.cajaAhorroPesos)
+            return this.cajaAhorroPesos;
+        } else if (caja === "dolares" && monto>0) {
+            if (this.cajaAhorroDolares >= 0) {
+                this.cajaAhorroDolares += monto;
+                return this.cajaAhorroDolares;
             } else {
-                return -1; // Tipo de caja no válido
+                return -1; // No tiene caja de ahorro en dólares
+            }
+        } else {
+            return -1; // Tipo de caja no válido
         }
     }
 
@@ -91,9 +92,9 @@ class Client{
 
 // Definición de clientes hardcodeados
 let clients = [
-    new Client(12345678, "pass1", "Juan", "García", 500, true),
-    new Client(87654321, "pass2", "María", "López", 800, false),
-    new Client(56789012, "pass3", "Carlos", "Martínez", 300, true),
-    new Client(34567890, "pass4", "Ana", "Sánchez", 1000, false),
-    new Client(90123456, "pass5", "Luis", "Rodríguez", 600, true)
+    new Client(12345678, "pass1", "Juan", "García",30000000), // tendría caja en dólares
+    new Client(87654321, "pass2", "María", "López", 29000000),// no tendría caja en dólares
+    new Client(56789012, "pass3", "Carlos", "Martínez", 1200000),// no tendría caja en dólares
+    new Client(34567890, "pass4", "Ana", "Sánchez", 1000000000),// tendría caja en dólares
+    new Client(90123456, "pass5", "Luis", "Rodríguez", 55000000)// tendría caja en dólares
 ];
--- functions.js
+++ functions.js
@@ -1,5 +1,14 @@
-const COTIZACION_DOLAR=1000000//dolar massita
-var userId = 1
+const COTIZACION_DOLAR=100000//dolar massita
+var clientId = 1
+var posCliente=encontrarClientePorID(clientId)
+changeScreen()
+clients[0].cajaAhorroPesos = 10000000
+clients[0].cajaAhorroDolares = 10
+cargarDropdowns();
+
+//
+refreshDinero(clients[posCliente].cajaAhorroPesos, clients[posCliente].cajaAhorroDolares, clients[posCliente].descubierto, clients[posCliente].descubiertoUsado)
+
 //ejercicio7
 function encontrarClientePorID(id) {
     for (let i = 0; i < clients.length; i++) {
@@ -10,7 +19,7 @@ function encontrarClientePorID(id) {
     return -1; // Si no se encuentra el cliente, devuelve -1
 }
 
-//ejercicio8
+//ejercicio8 FUNCIONA
 function encontrarCreditCardPorID(id) {
     for (let i = 0; i < creditCards.length; i++) {
         if (creditCards[i].id === id) {
@@ -20,7 +29,7 @@ function encontrarCreditCardPorID(id) {
     return -1; // Si no se encuentra la tarjeta, devuelve -1
 }
 
-//ejercicio9
+//ejercicio9 FUNCIONA
 function encontrarConsumptionPorID(id) {
     for (let i = 0; i < consumptions.length; i++) {
         if (consumptions[i].id === id) {
@@ -30,7 +39,7 @@ function encontrarConsumptionPorID(id) {
     return -1; // Si no se encuentra el consumo, devuelve -1
 }
 
-//ejercicio10
+//ejercicio10 FUNCIONA
 function encontrarTarjetasPorIDCliente(idCliente) {
     let tarjetasCliente = [];
     for (let i = 0; i < creditCards.length; i++) {
@@ -41,7 +50,7 @@ function encontrarTarjetasPorIDCliente(idCliente) {
     return tarjetasCliente;
 }
 
-//ejercicio11
+//ejercicio11 FUNCIONA
 function encontrarConsumosPorTarjeta(idTarjeta) {
     let consumosTarjeta = [];
     for (let i = 0; i < consumptions.length; i++) {
@@ -52,8 +61,161 @@ function encontrarConsumosPorTarjeta(idTarjeta) {
     return consumosTarjeta;
 }
 
-//ejercicio21 Linkear métodos
+//ejercicio21 Linkear métodos CompraVenta FUNCIONA
 function linkCompraVenta(){
-    monto
-    userId.compraVentaDolares(monto, cajaOrigen) 
+    monto=getMontoCompraVenta()
+    cajaOrigen=getMonedaCompraVenta()
+    if (clients[posCliente].compraVentaDolares(monto, cajaOrigen)){
+        mensaje="Se ha realizado la transacción con exito"
+    } else{
+        mensaje="La transacción ha fallado, chequea tu saldo y la caja de ahorro seleccionados"
+    }
+    putMensajeCompraVenta(mensaje)
+    refreshDinero(clients[posCliente].cajaAhorroPesos, clients[posCliente].cajaAhorroDolares, clients[posCliente].descubierto, clients[posCliente].descubiertoUsado)
+}
+
+//ejercicio21 Linkear métodos Extracción FUNCIONA
+function linkRetiro(){
+    monto=getMontoRetiroIngreso()
+    moneda=getMonedaRetiroIngreso()
+    if (clients[posCliente].extraerDinero(monto,moneda)){
+        mensaje="Extracción realizada"
+    }else{
+        mensaje="Extracción fallida"
+    }
+    putMensajeRetiroIngreso(mensaje)
+    refreshDinero(clients[posCliente].cajaAhorroPesos, clients[posCliente].cajaAhorroDolares, clients[posCliente].descubierto, clients[posCliente].descubiertoUsado)
+
+}
+
+//ejercicio21 Linkear métodos Ingreso FUNCIONA
+function linkIngreso(){
+    monto=getMontoRetiroIngreso()
+    moneda=getMonedaRetiroIngreso()
+    if (clients[posCliente].ingresarDinero(monto,moneda)>=0){
+        mensaje="Ingreso realizado"
+    }else{
+        mensaje="Ingreso fallido"
+    }
+    putMensajeRetiroIngreso(mensaje) 
+    refreshDinero(clients[posCliente].cajaAhorroPesos, clients[posCliente].cajaAhorroDolares, clients[posCliente].descubierto, clients[posCliente].descubiertoUsado)
+}
+
+//ejercicio 18, función login FUNCIONA
+function login(dni, password){
+    for (let i = 0; i < clients.length; i++) {
+        if (clients[i].dni == dni && clients[i].password==password) {
+            return clients[i].id; // Devuelve la posición del cliente en el vector
+        }
+    }
+    return -1
+}
+
+//ejercicio 18, linkear métodos de login FUNCIONA
+function linkLogin(){
+    dni=getDniUser()
+    password=getPasswordUser()
+    clientId=login(dni, password)
+    if (clientId>=0){
+        posCliente=encontrarClientePorID(clientId)
+        changeScreen()
+        cargarDropdowns();
+        window.alert("Usuario logeado")
+    }else{
+        window.alert("Ha ocurrido un error, intentalo de nuevo")
+    }
+}
+
+//ejercicio 19, función registro FUNCIONA
+function register(dni, password, nameUser, surname, ingresosAnuales){
+    if(dni<1000000){
+        return -1
+    }else if(password.length<5){
+        return -2
+    }else if(nameUser.length<3){
+        return -3
+    }else if(surname.length<3){
+        return -4
+    }else if(ingresosAnuales<100000){ //política bancaria-> mínimo 100000 anual
+        return -5
+    }else{
+        clients.push(new Client(dni, password, nameUser, surname, ingresosAnuales))
+        return idClient-1
+    }
+}
+
+//ejercicio 19, linkear métodos de registro FUNCIONA
+function linkRegister(){
+    dni=getDniUser()
+    password=getPasswordUser()
+    nameUser=getNameUser()
+    surname=getSurnameUser()
+    ingresosAnuales=getIngresosAnualesUser()
+    clientId=register(dni, password, nameUser, surname, ingresosAnuales)
+    if (clientId>0){
+        posCliente=encontrarClientePorID(clientId)
+        changeScreen()
+        cargarDropdowns();
+        window.alert("Usuario creado y logeado")
+    }else if(clientId==-1){
+        window.alert("DNI inválido")
+    }else if(clientId==-2){
+        window.alert("Contraseña muy corta")
+    }else if(clientId==-3){
+        window.alert("Nombre muy corto")
+    }else if(clientId==-4){
+        window.alert("Apellido muy corto")
+    }else if(clientId==-5){
+        window.alert("solo admitimos clientes con más de 100000 pesos de ganancias anuales POBRE")
+    }
+}
+
+//ejercicio 20, función de logout FUNCIONA
+function logout(){
+    clientId = -1
+    posCliente= -1
+    changeScreen()
+    window.alert("Ha cerrado su cuenta con éxito")
+}
+
+//ejercicio 15, sumar consumición FUNCIONA
+function addConsumption(idCreditCard, local, monto){
+    var posCreditCard =encontrarCreditCardPorID(idCreditCard)
+    if (creditCards[posCreditCard].fechaVencimiento> new Date()){
+        creditCards[posCreditCard].saldo+=monto
+        consumptions.push(new Consumption(idCreditCard, local, monto))
+        return true
+    }
+    return false
+}
+
+//ejercicio 30, conectar métodos para sumar consumicion FUNCIONA
+function linkAddConsumption() {
+    let idCreditCard = getCreditCardConsumption();
+    let local = getLocalConsumption();
+    let monto = getMontoConsumption();
+
+    if (addConsumption(idCreditCard, local, monto)) {
+        alert("Consumo agregado correctamente.");
+        clearConsumptionInputs();
+    } else {
+        alert("Error al agregar el consumo. Verifique los datos e intente nuevamente.");
+    }
+}
+
+//ejercicio 31, conectar métodos para pagar el saldo FUNCIONA
+function linkPayCreditCard() {
+    var idCreditCard = getCreditCardPay();
+    var monto = getMontoPay();
+    var posTarjeta=encontrarCreditCardPorID(idCreditCard);
+    var pago = creditCards[posTarjeta].pay(monto);
+    if (pago==1){
+        mensaje="Se realizo el pago total de la tarjeta"
+    } else if(pago==0){
+        mensaje="Se realizo el pago parcial de la tarjeta"
+    }else{
+        mensaje="No se pudo realizar el pago de la tarjeta"
+    }
+    refreshDinero(clients[posCliente].cajaAhorroPesos, clients[posCliente].cajaAhorroDolares, clients[posCliente].descubierto, clients[posCliente].descubiertoUsado)
+    putMensajePayCreditCard(mensaje)
 }
